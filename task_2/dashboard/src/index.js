9b05f8acb37525799d0d67a1a1e8d49b2c44a68f
OGOR-STEPHEN <ogorstephen485@gmail.com>
2025-01-03 18:05:59 +0100

    Second commit

---
+++ task_2/dashboard/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App';
+import Notifications from "./Notifications";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Notifications />, document.getElementById("root-notifications"));
